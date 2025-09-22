41ceb351f53822615a0349aaf5f4f2d874e8c6c8
JohnGallemore <122250845+JohnGallemore@users.noreply.github.com>
2025-09-22 12:43:14 -0500

    Update Talk.go
    I went through and made some small changes to the serverMode function and some big changes to the clientMode function. Theoretically, this change should allow for non blocking communication on the side of the client. Need to work on server side.

--- Talk.go
+++ Talk.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -35,24 +36,78 @@ func main() {
 
 func clientMode() {
 
-	// Establish a connection by tryign to connect to a provided IP address.
+	// Establish a connection by trying to connect to a provided IP address.
 	// Currently only using localhost:8080 for testing
 	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		log.Println(err)
 	}
 	defer conn.Close()
+	fmt.Println("Client: Connection established.")
 
-	reader := bufio.NewReader(os.Stdin)
+	readChan := make(chan []byte)
+	writeChan := make(chan []byte)
+	quitChan := make(chan bool)
+	errChan := make(chan error)
 
+	// Infinite loop until a break command is received
+CONNECTION_LOOP:
 	for {
-		msg, err := reader.ReadString('\n')
-		if err != nil {
-			log.Println(err)
-		}
+		// A go function that reads data from the connection into a buffer
+		// Once the data from the connection has been read into the buffer,
+		// send the data from the buffer into the read channel.
+		go func() {
+			buffer := make([]byte, 1024)
 
-		conn.Write([]byte(msg))
+			n, err := conn.Read(buffer)
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			readChan <- buffer[:n]
+		}()
+
+		// A go function to read input from the user into a buffer.
+		// Once data has been read from input, it is sent to the write channel,
+		// where it can then be written to the connection in the select statement.
+		go func() {
+			buffer := make([]byte, 1024)
+
+			reader := bufio.NewReader(os.Stdin)
+
+			n, err := reader.Read(buffer)
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			if strings.EqualFold(string(buffer[:n]), "quit") {
+				quitChan <- strings.EqualFold(string(buffer[:n]), "quit")
+				return
+			}
+
+			writeChan <- buffer[:n]
+		}()
+
+		// A select statement that waits until there is something in one of the channels
+		// to do something with.
+		select {
+		case readData := <-readChan:
+			fmt.Println("Message Received: ", string(readData))
+
+		case writeData := <-writeChan:
+			conn.Write(writeData)
+
+		case errData := <-errChan:
+			fmt.Println("Error occurred: ", errData)
+
+		case quitData := <-quitChan:
+			fmt.Println("Ending Connection: ", quitData)
+			break CONNECTION_LOOP
+		}
 	}
+
 }
 
 func serverMode() {
@@ -64,15 +119,16 @@ func serverMode() {
 	}
 	defer ln.Close()
 
-	for {
+	go func() {
 		//Accept incoming connection
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Println()
+			log.Println("Error accepting connection: ", err)
+			return
 		}
 
-		go handleConnection(conn)
-	}
+		conn.Write([]byte("Server: Connection established."))
+	}()
 }
 
 func handleConnection(conn net.Conn) {
@@ -92,7 +148,8 @@ func handleConnection(conn net.Conn) {
 				log.Println(err)
 				break
 			}
-		} else if n > 0 { //Check if the int returned by reading the connection is more than 0. If it is, there is something to write.
+		} else if n > 0 {
+			//Check if the int returned by reading the connection is more than 0. If it is, there is something to write.
 			//Print what has been read into the byte buffer.
 			//The statement buffer[:n] will read everything in the buffer up to but excluding the nth element
 			fmt.Println(string(buffer[:n]))
