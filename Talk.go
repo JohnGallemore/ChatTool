82b6456b4d6494c84fbf4d8342c671f08ccc9659
JohnGallemore <122250845+JohnGallemore@users.noreply.github.com>
2025-09-24 13:57:59 -0500

    Broke Talk.go
    I broke it. The primary issue lies in the fat that the go routines are defined and used in the for loop, leading to unchecked instantiation of new go routines, leading to complete cpu utilization, which leads to a system crash. Those go functions need to be moved, and I need to abstract my communication handling out of the client and server functions and into its own function.

--- Talk.go
+++ Talk.go
@@ -7,7 +7,7 @@ import (
 	"net"
 	"os"
 	"strings"
-	"time"
+	"sync"
 )
 
 func main() {
@@ -44,7 +44,7 @@ func clientMode() {
 		log.Println(err)
 	}
 	defer conn.Close()
-	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	//conn.SetDeadline(time.Now().Add(5 * time.Second)) //Set the read and write deadline to 5 seconds so that the
 	conn.Write([]byte("Client: Connection established."))
 
 	readChan := make(chan []byte)
@@ -52,13 +52,22 @@ func clientMode() {
 	quitChan := make(chan bool)
 	errChan := make(chan error)
 
+	// A mutex var so that we can lock while the read function is working
+	var muRead sync.Mutex
+
+	// A mutex var so that we can lock while the write function is working
+	var muWrite sync.Mutex
+
 	// Infinitely loop until a break command is received
-CONNECTION_LOOP:
+COMM_LOOP:
 	for {
 		// A go function that reads data from the connection into a buffer
 		// Once the data from the connection has been read into the buffer,
 		// send the data from the buffer into the read channel.
 		go func() {
+			muRead.Lock()
+			defer muRead.Unlock()
+			fmt.Println("We have entered reading mode")
 			buffer := make([]byte, 1024)
 
 			n, err := conn.Read(buffer)
@@ -74,6 +83,9 @@ CONNECTION_LOOP:
 		// Once data has been read from input, it is sent to the write channel,
 		// where it can then be written to the connection in the select statement.
 		go func() {
+			muWrite.Lock()
+			defer muWrite.Unlock()
+			fmt.Println("We have entered writing mode")
 			buffer := make([]byte, 1024)
 
 			reader := bufio.NewReader(os.Stdin)
@@ -102,16 +114,21 @@ CONNECTION_LOOP:
 			conn.Write(writeData)
 
 		case errData := <-errChan:
-			if netErr, ok := errData.(net.Error); ok && netErr.Timeout() {
-				log.Println("Timeout Error: ", errData)
-			} else {
-				fmt.Println("Error occurred: ", errData)
-				panic(errData)
-			}
+			/*
+				if netErr, ok := errData.(net.Error); ok && netErr.Timeout() {
+					log.Println("Timeout Error: ", errData)
+					continue COMM_LOOP
+				} else {
+					fmt.Println("Error occurred: ", errData)
+					log.Fatalln(errData)
+				}*/
+
+			fmt.Println("Error occurred: ", errData)
+			log.Fatalln(errData)
 
 		case quitData := <-quitChan:
 			fmt.Println("Ending Connection: ", quitData)
-			break CONNECTION_LOOP
+			break COMM_LOOP
 
 		default:
 		}
@@ -132,6 +149,12 @@ func serverMode() {
 	quitChan := make(chan bool)
 	errChan := make(chan error)
 
+	// A mutex var so that we can lock while the read function is working
+	var muRead sync.Mutex
+
+	// A mutex var so that we can lock while the write function is working
+	var muWrite sync.Mutex
+
 COMM_LOOP:
 	for {
 		//Accept incoming connection
@@ -141,13 +164,16 @@ COMM_LOOP:
 			return
 		}
 		defer conn.Close()
-		conn.SetDeadline(time.Now().Add(5 * time.Second))
+		// conn.SetDeadline(time.Now().Add(5 * time.Second))
 		conn.Write([]byte("Server: Connection established."))
 
 		// A go function that reads data from the connection into a buffer
 		// Once the data from the connection has been read into the buffer,
 		// send the data from the buffer into the read channel.
 		go func() {
+			muRead.Lock()
+			defer muRead.Unlock()
+			fmt.Println("We have entered reading mode")
 			buffer := make([]byte, 1024)
 
 			n, err := conn.Read(buffer)
@@ -163,6 +189,9 @@ COMM_LOOP:
 		// Once data has been read from input, it is sent to the write channel,
 		// where it can then be written to the connection in the select statement.
 		go func() {
+			muWrite.Lock()
+			defer muWrite.Unlock()
+			fmt.Println("We have entered writing mode")
 			buffer := make([]byte, 1024)
 
 			reader := bufio.NewReader(os.Stdin)
@@ -191,12 +220,17 @@ COMM_LOOP:
 			conn.Write(writeData)
 
 		case errData := <-errChan:
-			if netErr, ok := errData.(net.Error); ok && netErr.Timeout() {
-				log.Println("Timeout Error: ", errData)
-			} else {
-				fmt.Println("Error occurred: ", errData)
-				panic(errData)
-			}
+			/*
+				if netErr, ok := errData.(net.Error); ok && netErr.Timeout() {
+					log.Println("Timeout Error: ", errData)
+					continue COMM_LOOP
+				} else {
+					fmt.Println("Error occurred: ", errData)
+					log.Fatalln(errData)
+				}*/
+
+			fmt.Println("Error occurred: ", errData)
+			log.Fatalln(errData)
 
 		case quitData := <-quitChan:
 			fmt.Println("Ending Connection: ", quitData)
@@ -206,3 +240,104 @@ COMM_LOOP:
 		}
 	}
 }
+
+/*
+func handleComm(conn net.Conn){
+	defer conn.Close()
+
+	readChan := make(chan []byte)
+	writeChan := make(chan []byte)
+	quitChan := make(chan bool)
+	errChan := make(chan error)
+
+	// A mutex var so that we can lock while the read function is working
+	var muRead sync.Mutex
+
+	// A mutex var so that we can lock while the write function is working
+	var muWrite sync.Mutex
+
+	COMM_LOOP:
+	for {
+		//Accept incoming connection
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err)
+			return
+		}
+		defer conn.Close()
+		// conn.SetDeadline(time.Now().Add(5 * time.Second))
+		conn.Write([]byte("Server: Connection established."))
+
+		// A go function that reads data from the connection into a buffer
+		// Once the data from the connection has been read into the buffer,
+		// send the data from the buffer into the read channel.
+		go func() {
+			muRead.Lock()
+			defer muRead.Unlock()
+			fmt.Println("We have entered reading mode")
+			buffer := make([]byte, 1024)
+
+			n, err := conn.Read(buffer)
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			readChan <- buffer[:n]
+		}()
+
+		// A go function to read input from the user into a buffer.
+		// Once data has been read from input, it is sent to the write channel,
+		// where it can then be written to the connection in the select statement.
+		go func() {
+			muWrite.Lock()
+			defer muWrite.Unlock()
+			fmt.Println("We have entered writing mode")
+			buffer := make([]byte, 1024)
+
+			reader := bufio.NewReader(os.Stdin)
+
+			n, err := reader.Read(buffer)
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			if strings.EqualFold(string(buffer[:n]), "quit") {
+				quitChan <- strings.EqualFold(string(buffer[:n]), "quit")
+				return
+			}
+
+			writeChan <- buffer[:n]
+		}()
+
+		// A select statement that waits until there is something in one of the channels
+		// to do something with.
+		select {
+		case readData := <-readChan:
+			fmt.Println(string(readData))
+
+		case writeData := <-writeChan:
+			conn.Write(writeData)
+
+		case errData := <-errChan:
+			/*
+				if netErr, ok := errData.(net.Error); ok && netErr.Timeout() {
+					log.Println("Timeout Error: ", errData)
+					continue COMM_LOOP
+				} else {
+					fmt.Println("Error occurred: ", errData)
+					log.Fatalln(errData)
+				}*/ /*
+
+			fmt.Println("Error occurred: ", errData)
+			log.Fatalln(errData)
+
+		case quitData := <-quitChan:
+			fmt.Println("Ending Connection: ", quitData)
+			break COMM_LOOP
+
+		default:
+		}
+	}
+}*/
